680f26b789e254d61a85ec94f130a3764d9baf63
agahkarakuzu <agahkarakuzu@gmail.com>
2019-03-04 11:56:29 -0800

    first one

---
+++ client.c
@@ -0,0 +1,51 @@
+// This program connects to the socket /tmp/rth2qmrlab and sends message to that socket. 
+
+#include <sys/types.h>
+#include <sys/socket.h> 
+#include <sys/un.h>
+#include <stdio.h> 
+#include <string.h>
+
+#define DATA "Hello from the othersiiiiideee."
+
+#define SERVER_PATH "/tmp/rth2qmrlab"
+#define BUFFER_LENGTH 1024
+#define FALSE 0 
+
+
+void main(int argc, char *argv[]){
+
+    int sd =-1, rc, bytesReceived, sock;
+    char buffer[BUFFER_LENGTH];
+    struct sockaddr_un server; 
+
+    memset(&serveraddr, 0, sizeof(server));
+    sock = socket(AF_UNIX, SOCK_STREAM, 0);
+
+    if (sock<0){
+        perror("Cannot open socket.");
+        exit(1);
+    }
+
+    if (argc > 1)
+         strcpy(serveraddr.sun_path, argv[1]);
+      else
+         strcpy(serveraddr.sun_path, SERVER_PATH);
+
+    // Establish a connection to the server. 
+
+    rc = connect(sock, (struct sockaddr *)&server, SUN_LEN(&server));
+
+    if (rc<0){
+        perror("Connection to the server failed.");
+        exit(1);
+    }
+
+    rc = write(sock, DATA, sizeof(DATA));
+
+    if(rc < 0){
+        perror("Cannot write on the stream socket.");
+    }
+
+    close(sock);
+}
---
+++ server.c
@@ -0,0 +1,92 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <string.h>
+#include <stdio.h>
+
+
+#define SERVER_PATH     "/tmp/rth2qmrlab"
+#define BUFFER_LENGTH    1024
+#define FALSE              0
+
+// I need kinda MATLAB version of this. 
+
+void main()
+{
+    int sock, msgsock, rval, rc;
+    struct sockaddr_un server; // server is a structture that store addresses for UNIX domain sockets.
+    char buf[BUFFER_LENGTH];
+
+    // Create the socket here 
+
+    // This function returns a socket descriptor, which represents an endpoint. It also identified that the UNIX
+    // address family (AF_UNIX) with the stream transport (SOCK_STREAM) will be used for the socket. 
+
+    sock = socket(AF_UNIX, SOCK_STREAM, 0);
+    
+    if (sock < 0)
+    {
+        perror("Cannot open the socket.");
+        exit(1);
+    }
+
+    memset(&server, 0, sizeof(server));
+    server.sun_family = AF_UNIX;
+    strcpy(server.sun_path, SERVER_PATH); // This is created as a socker in the system filename space. 
+
+    // The file should be deleted by the caller when it is no longer needed. 
+
+    // Command in the if statement is for binding. 
+
+    // Bind gets a unique name for the socket. 
+
+    rc = bind(sock, (struct sockaddr *)&server, SUN_LEN(server));
+
+    if (rc < 0)
+    {
+        perror("Binding stream socket");
+        exit(1);
+    }
+
+    printf("Socket has name %s\n", server.sun_path);
+
+    // The listen function allows the server to accept incoming client connections. Here, backlog is set to 5.
+    // This means that the system will que 5 incoming connection requests before the system starts rejecting incoming
+    // requests. 
+
+    rc = listen(sock, 10);
+    
+    if (rc<0){
+
+        perror("Listen() failed.");
+        break;
+    }
+
+    for (;;) // Infinite loop 
+    {
+        // Accept an incoming request. This call will block indefinetely waiting for the incoming connection to arrive.
+
+        msgsock = accept(sock, NULL, NULL);
+
+        if (msgsock < 0)
+
+            perror("Accept() failed.");
+
+        else
+
+            do
+            {
+                bzero(buf, sizeof(buf));
+                if ((rval = read(msgsock, buf, BUFFER_LENGTH)) < 0)
+                    perror("reading stream message");
+                else if (rval == 0)
+                    printf("Ending connection\n");
+                else
+                    printf("-->%s\n", buf);
+            } while (rval > 0);
+        close(msgsock);
+    }
+
+    close(sock);
+    unlink(SERVER_PATH);
+}
